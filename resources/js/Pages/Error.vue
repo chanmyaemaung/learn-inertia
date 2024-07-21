19e84f2f536e8958075dde284245d11bfc96e68e
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-07-21 21:43:24 +0700

    Returning an Inertia Response

---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,40 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import { computed } from "vue";
+
+const props = defineProps({
+    status: {
+        type: Number,
+        required: true,
+    },
+});
+
+const title = computed(() => {
+    return (
+        {
+            404: "Page Not Found",
+            403: "403 | Forbidden",
+        }[props.status] || "An Error Occurred"
+    );
+});
+
+const description = computed(() => {
+    return (
+        {
+            404: "The page you are looking for does not exist.",
+            403: "You are not authorized to access this page.",
+        }[props.status] || "An error occurred."
+    );
+});
+</script>
+
+<template>
+    <Head :title="title" />
+
+    <GuestLayout>
+        <div class="font-medium text-red-600 text-center">
+            {{ description }}
+        </div>
+    </GuestLayout>
+</template>
